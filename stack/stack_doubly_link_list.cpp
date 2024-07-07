89970b3e92b04ae83a4f809a663a91fc5ad7182b
abdulmalek.swe.585@gmail.com <abdulmale.swe.585@gmail.com>
2024-07-07 09:16:11 +0600

    feat:create stack implementation

---
+++ stack/stack_doubly_link_list.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int value;
+    Node *next;
+    Node *prev;
+    Node(int value){
+        this->value=value;
+        this->next=NULL;
+        this->prev=NULL;
+    }
+};
+class myStack
+{
+public:
+     Node *head = NULL;
+     Node *tail = NULL;
+     int sz = 0;
+    void push(int val)
+    {
+    //    insert_at_tail(head,tail,val); 
+    sz++;
+      Node *newNode = new Node(val);
+      if(head==NULL && tail ==NULL){
+        head =newNode;
+        tail=newNode;
+        return;
+      }
+       tail->next = newNode;
+    newNode->prev = tail;
+    tail = tail->next;
+    }
+    void pop()
+    {
+        sz--;
+       Node *deleteNode = tail;
+    tail=tail->prev;
+    if(tail==NULL) head = NULL;
+    return;
+    delete deleteNode;
+    tail->next=NULL;
+    }
+    int top()
+    {
+        return tail->value;
+    }
+    int size()
+    {
+        return sz;
+    }
+    bool empty()
+    {
+        if (sz== 0)
+            return true;
+        else
+            return false;
+    }
+};
+int main()
+{
+    myStack st;
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        st.push(i*10);
+    }
+    while (!st.empty())
+    {
+        cout<<st.top()<<" ";
+        st.pop();
+  
+    }
+    
+    return 0;
+}
---
+++ stack/stl_in_list.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    stack<int>st;
+    st.push(12);
+    cout<<st.top();
+    return 0;
+}
